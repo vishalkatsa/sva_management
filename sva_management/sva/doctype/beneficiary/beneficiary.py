a2dac0539b04fee00160f49584c496b7443f04ac
biharpediav1 <biharpedia.in@gmail.com>
2024-06-20 18:03:15 +0530

    hi

--- sva_management/sva/doctype/beneficiary/beneficiary.py
+++ sva_management/sva/doctype/beneficiary/beneficiary.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, vk and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
 
 class Beneficiary(Document):
 	pass
+
+@frappe.whitelist(allow_guest=True)
+def get_beneficiary_by_id(name):
+
+	beneficiary = frappe.get_doc('Beneficiary', name)
+	return beneficiary
